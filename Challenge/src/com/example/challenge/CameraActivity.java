a696497e85fa1270af51c3a8ca948cd4a2f1f269
Daiji Nishimura <bundai223@gmail.com>
2013-10-28 02:11:40 +0900

    画面遷移まで実装

--- Challenge/src/com/example/challenge/CameraActivity.java
+++ Challenge/src/com/example/challenge/CameraActivity.java
@@ -1,12 +1,11 @@
 package com.example.challenge;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class CameraActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,11 +13,10 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.back, menu);
         return true;
     }
 
@@ -26,12 +24,9 @@ public class MainActivity extends Activity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-        case R.id.activity_camera:
-        	startActivity(new Intent(MainActivity.this, CameraActivity.class));
+        case R.id.back:
+        	finish();
             break;
-        case R.id.activity_gl:
-        	startActivity(new Intent(MainActivity.this, GLActivity.class));
-        	break;
         }
         return super.onOptionsItemSelected(item);
     }
